e604003177cb00a232b4bf0212167e8ae6f53245
lorna gathumbi <nyawiralornah@gmail.com>
2024-12-01 10:15:20 +0300

    python init_self

--- lib/dog.py
+++ lib/dog.py
@@ -2,3 +2,13 @@
 
 class Dog:
     pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def adopted(self, owner_name):
+        self.owner = owner_name        
+        pass
